6114e7343ec6acc209bf4d5253169908e1bfacc9
Maksim Ivanov <satansdeer@gmail.com>
2020-04-19 11:11:06 +0200

    Add ssr

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+
+import App from "../src/App";
+
+const PORT = 8000;
+
+const app = express();
+
+app.use("^/$", (req, res, next) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    );
+  });
+});
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')))
+
+app.listen(PORT, () => {
+  console.log(`App launched on ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </>
   );
 }
 
